30e0e86cdd1ad337d10c60c1a41d9887b2c9cc14
AayanFazal <aayan.fazal07@gmail.com>
2023-12-20 18:06:40 -0500

    Added new routes

--- server/index.js
+++ server/index.js
@@ -52,11 +52,46 @@ app.get('/api/:comp/:teamNum', (req, res) => {
         console.error('Error executing query:', err);
         return res.status(500).json({ error: 'Internal Server Error' });
       }
+      
   
       // Send the data as JSON to the client
       res.json(results);
+      console.log(results); 
     });
   });
+
+//Returns all raw data from comp
+app.get('/api/:comp/all/raw', (req, res) => {
+  const getComp = req.params.comp; 
+  connection.query('SELECT * FROM ??;',[getComp],(err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    
+
+    // Send the data as JSON to the client
+    res.json(results);
+  });
+});
+
+//compare 
+app.get('/api/:comp/compare/:team1/:team2/:team3', (req, res) => {
+  const getComp = req.params.comp; 
+  const team1 = req.params.team1; 
+  const team2 = req.params.team2; 
+  const team3 = req.params.team3; 
+  connection.query('SELECT * FROM ?? WHERE teamNum IN (?, ?, ?);',[getComp,team1,team2,team3],(err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    
+
+    // Send the data as JSON to the client
+    res.json(results);
+  });
+});
 
 
 // Start the server
