163e7c14f9c68b6af747167e64e22033d57b8f73
dmanslick <dvvshah@gmail.com>
2023-12-23 16:07:57 -0500

    changed all fetch options

--- client/app/page.js
+++ client/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     const [competitions, setCompetitions] = useState(false)
 
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/comp`, { method: 'GET', mode: 'cors' })
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/comp`, { method: 'GET' })
             .then(res => {
                 if (!res.ok) {
                     return false
