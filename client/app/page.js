508acd60a747c7aaa73fe7b832331811f33d4bac
dmanslick <dvvshah@gmail.com>
2023-12-25 15:03:51 -0500

    updated layout in home page

--- client/app/page.js
+++ client/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { Anchor, Center, Divider, Flex, Group, List, Loader, Text } from '@mantine/core'
+import { Anchor, Center, List, Loader, Table, Text } from '@mantine/core'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
@@ -22,22 +22,31 @@ export default function Home() {
     if (competitions) {
         return (
             <>
-                <Flex align='center' direction='column'>
+                <Center>
                     <Text fz='xl' mt='lg'>Competitions:</Text>
-                    {competitions.map((competition, index) => {
-                        return (
-                            <Group justify='center' mt='lg' key={index}>
-                                <div>
-                                    <Anchor component={Link} href={competition.Tables_in_scoutschema.toString()}>{competition.Tables_in_scoutschema}</Anchor>
-                                </div>
-                                <Divider orientation='vertical' size='xs' />
-                                <div>
-                                    <Anchor component={Link} href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/download-data/${competition.Tables_in_scoutschema}`}>Download CSV Data</Anchor>
-                                </div>
-                            </Group>
-                        )
-                    })}
-                </Flex>
+                    <Table>
+                        <Table.Thead>
+                            <Table.Tr>
+                                <Table.Th>Competition</Table.Th>
+                                <Table.Th>CSV Download</Table.Th>
+                            </Table.Tr>
+                        </Table.Thead>
+                        <Table.Tbody>
+                            {competitions.map((competition, index) => {
+                                return (
+                                    <Table.Tr key={index}>
+                                        <Table.Th>
+                                            <Anchor component={Link} href={competition.Tables_in_scoutschema.toString()}>{competition.Tables_in_scoutschema}</Anchor>
+                                        </Table.Th>
+                                        <Table.Th>
+                                            <Anchor component={Link} href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/download-data/${competition.Tables_in_scoutschema}`}>Download CSV Data</Anchor>
+                                        </Table.Th>
+                                    </Table.Tr>
+                                )
+                            })}
+                        </Table.Tbody>
+                    </Table>
+                </Center>
             </>
         )
     }
